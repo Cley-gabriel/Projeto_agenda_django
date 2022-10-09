fd98f68fc86eadd15530f494a0e04a4c8cb06956
Cley-gabriel <gabrielcley23@hotmail.com>
2022-10-08 21:51:12 -0300

    Criação dos models

--- contatos/models.py
+++ contatos/models.py
@@ -1,3 +1,21 @@
+from email.policy import default
+from pickle import TRUE
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length = 100)
+
+class Contato(models.Model):
+    nome = models.CharField(max_length = 30)
+    sobrenome = models.CharField(max_length = 70, blank = True)
+    telefone = models.CharField(max_length = 12)
+    email = models.CharField(max_length = 60, blank = True)
+    data_criação = models.DateTimeField(default = timezone.now)
+    descricao = models.TextField(blank = True)
+    categoria = models.ForeignKey(Categoria, on_delete = models.DO_NOTHING)
+
+
+
