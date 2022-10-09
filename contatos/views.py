3ad0463fba7a451a92f6cf447b0baef4c6478093
Cley-gabriel <gabrielcley23@hotmail.com>
2022-10-09 10:03:09 -0300

    commit

--- contatos/views.py
+++ contatos/views.py
@@ -1,3 +1,4 @@
+from urllib import request
 from django.shortcuts import render
 from .models import Contato
 
@@ -6,4 +7,10 @@ def index(request):
     contatos = Contato.objects.all()
     return render (request, 'contatos/index.html', {
         'contatos': contatos
+    })
+
+def ver_contato(resquest, contato_id):
+    contato = Contato.objects.get(id=contato_id)
+    return render (resquest, 'contatos/ver_contato.html', {
+        'contato': contato
     })
