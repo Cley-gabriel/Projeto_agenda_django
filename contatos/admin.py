aef60388e957e5777a5fb4342cab76e3303bd52b
Cley-gabriel <gabrielcley23@hotmail.com>
2022-10-08 22:38:06 -0300

    Trabalhando na parte administrativa do django

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Contato, Categoria
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id','nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria')
+    list_display_links = ('id', 'nome', 'sobrenome')
+    list_filter = ('nome', 'sobrenome')
+    list_per_page = 1
+    search_fields = ('nome', 'sobrenome')
 
 # Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
--- contatos/models.py
+++ contatos/models.py
@@ -5,14 +5,20 @@ from django.utils import timezone
 class Categoria(models.Model):
     nome = models.CharField(max_length = 100)
 
+    def __str__(self) :
+        return self.nome
+
 class Contato(models.Model):
     nome = models.CharField(max_length = 30)
     sobrenome = models.CharField(max_length = 70, blank = True)
     telefone = models.CharField(max_length = 12)
     email = models.CharField(max_length = 60, blank = True)
-    data_criação = models.DateTimeField(default = timezone.now)
+    data_criacao = models.DateTimeField(default = timezone.now)
     descricao = models.TextField(blank = True)
     categoria = models.ForeignKey(Categoria, on_delete = models.DO_NOTHING)
+
+    def __str__(self) :
+        return self.nome
 
 
 
