cbf071771e27c3280c3b060897ea164d2db5d739
Cley-gabriel <gabrielcley23@hotmail.com>
2022-10-09 09:15:50 -0300

    Exibindo valores na views

--- contatos/admin.py
+++ contatos/admin.py
@@ -4,8 +4,7 @@ from .models import Contato, Categoria
 class ContatoAdmin(admin.ModelAdmin):
     list_display = ('id','nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria')
     list_display_links = ('id', 'nome', 'sobrenome')
-    list_filter = ('nome', 'sobrenome')
-    list_per_page = 1
+    list_per_page = 10
     search_fields = ('nome', 'sobrenome')
 
 # Register your models here.
