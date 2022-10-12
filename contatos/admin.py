cfeac16af00b3a9dd04d6bac7eca013adb9ccbd4
Cley-gabriel <gabrielcley23@hotmail.com>
2022-10-11 21:47:46 -0300

    Adição de pg na agenda

--- contatos/admin.py
+++ contatos/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from .models import Contato, Categoria
 
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('id','nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria')
+    list_display = ('id','nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria', 'mostrar')
     list_display_links = ('id', 'nome', 'sobrenome')
     list_per_page = 10
     search_fields = ('nome', 'sobrenome')
+    list_editable = ('telefone', 'mostrar')
 
 # Register your models here.
 admin.site.register(Categoria)
