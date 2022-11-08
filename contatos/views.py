f51655e28e2189c3f5491c500eef6f3506ecf171
Cley-gabriel <gabrielcley23@hotmail.com>
2022-11-08 20:56:56 -0300

    Adicionando Mensagens com Django Messages

--- contatos/views.py
+++ contatos/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
 from .models import Contato
 from django.core.paginator import Paginator
 from django.db.models import Q, Value
 from django.db.models.functions import Concat
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
@@ -37,7 +38,11 @@ def busca (request):
     termo = request.GET.get('termo')
 
     if termo is None or not termo:
-        raise Http404
+        messages.add_message(request, messages.ERROR, 'Campo termo não pode ficar vazio')
+        return redirect('index')
+    if termo:
+        messages.add_message(request, messages.SUCCESS, 'Contato encontrado')
+        
 
     campos = Concat('nome', Value (' '), 'sobrenome')
 
