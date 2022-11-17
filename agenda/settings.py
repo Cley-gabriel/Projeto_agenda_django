1c6b473a3fb8228255e6fe298f0f230f384c2b18
Cley-gabriel <gabrielcley23@hotmail.com>
2022-11-17 10:09:06 -0300

    Coletando os arquivos static

--- agenda/settings.py
+++ agenda/settings.py
@@ -124,6 +124,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/static')
 ]
 
+STATIC_ROOT = os.path.join('static')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
 
